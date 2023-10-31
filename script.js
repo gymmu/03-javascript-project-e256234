592f55d5f481683b061e4ba0af6acd72fc521c43
zoe <e256234@edu.sbl.ch>
2023-10-31 11:17:04 +0100

    Aufgabe 03

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ export function aufgabe02(args) {
  }
   return result.join("")
 }
+export function aufgabe03(args){
+  const input = args
+  const result = []
+  let count = 0
+  for (let i=0; i < input.length; i++){
+    const currentElement = input[i]
+    if ( currentElement === "e"){
+    count = count + 1 
+    } else if (currentElement === "E"){
+    count = count + 1
+    
+  }
+  }
+  return count
+}
